89edafc1db8c00c98a2a585e2d6b0c8572ab1c06
Neetu Turan <turannu@mail.uc.edu>
2024-11-16 02:05:39 -0500

    Validation of  BrewerySchema

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SipsandBites;
+using SipsBites;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
 namespace XML_Project.Pages
 {
     public class IndexModel : PageModel
     {
+        static readonly HttpClient client = new HttpClient();
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -22,9 +26,33 @@ namespace XML_Project.Pages
                 brand = inBrand;
             }
             ViewData["Brand"] = brand;
-            
-            
+            var task = client.GetAsync("https://api.openbrewerydb.org/breweries");
+            HttpResponseMessage result = task.Result;
+            List<Brewery> breweries = new List<Brewery>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("Data/breweryschema.json"));
+                JArray jsonarray = JArray.Parse(jsonString);
+                IList<string> validationEvents = new List<string>();
+                if (jsonarray.IsValid(schema, out validationEvents))
+                {
+                    breweries = Brewery.FromJson(jsonString);
+                }
+                else
+                {
+                    foreach (string evt in validationEvents)
+                    {
 
+                        Console.WriteLine(evt);
+                    }
+                }
+                ViewData["Breweries"] = breweries;
+
+
+
+
+            }
         }
-    }
-}
+} }
