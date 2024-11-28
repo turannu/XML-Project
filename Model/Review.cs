1c7c882d172999acc272f55ee5290e141c8c8e47
Adenike_driod <adenirae@mail.uc.edu>
2024-11-28 14:13:46 -0800

    I added resturants name field to review Form

--- Model/Review.cs
+++ Model/Review.cs
@@ -2,6 +2,11 @@ public class Review
 {
     public string ReviewerName { get; set; }
     public string BreweryName { get; set; }
+
+
+    public string ResturantName { get; set; }
     public string ReviewText { get; set; }
     public int Rating { get; set; }
 }
+
+
